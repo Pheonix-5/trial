16b4943c445e4645b6ea0940ac2e9c91c6354cd7
Phoenix_sharma <82761151+Pheonix-5@users.noreply.github.com>
2023-09-19 21:56:55 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+        <span>Copyright All Right Reserved 2022, Shree Ganpati Educational Society</span>
+        <span>Designed By - <a href="https://akshaysharmaportfolio.site/">AKSHAY SHARMA</a></span>
+</footer>
