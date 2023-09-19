d77c4c05e40aa5384aa22f3c221962fd96930346
Phoenix_sharma <82761151+Pheonix-5@users.noreply.github.com>
2023-09-19 21:56:10 +0530

    Delete sanskar sports club directory

--- sanskar sports club/footer.php
+++
@@ -1,4 +0,0 @@
-<footer>
-        <span>Copyright All Right Reserved 2022, Shree Ganpati Educational Society</span>
-        <span>Designed By - <a href="https://akshaysharmaportfolio.site/">AKSHAY SHARMA</a></span>
-</footer>
